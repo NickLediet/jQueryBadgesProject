ba8de7734061606f323b49c58da87606f6516d5e
NickLediet <nicklediet@gmail.com>
2017-03-11 20:32:16 -0500

    Finished the project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,17 @@
 $(function() {
 
-  // your code will go here
+  // Request report card object from codeschool api jsonp
+  $.ajax({
+    url: 'https://www.codeschool.com/users/1949742.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      var badges = $('#badges');
+      response.courses.completed.forEach(function(course){
+        badges.append('<div class="course"><h3>'+ course.title +'</h3>'+
+        '<img src="'+course.badge+'" />'+
+        '<a href="'+course.url+'" target="_blank" class="btn btn-primary">See Course</a>'+'</div>');
+      });
+    }
+  });
 
 });
